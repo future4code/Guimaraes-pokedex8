900ad486e4e81636c5c516c37095187b90890997
Lucas <lucascardoso0221@gmail.com>
2022-04-13 01:47:12 -0300

    Card de pokémon finalizados

--- src/App.js
+++ src/App.js
@@ -2,27 +2,12 @@ import styled from 'styled-components'
 import GlobalStyle from './globalStyles'
 import PokemonCard from './components/PokemonCard'
 
-const Container = styled.div`
-  display: flex;
-`
-
 const App = () => {
   return (
     <>
     <GlobalStyle />
 
     <Container>
-      <PokemonCard />
-      <PokemonCard />
-      <PokemonCard />
-      <PokemonCard />
-      <PokemonCard />
-      <PokemonCard />
-    </Container>
-    <Container>
-      <PokemonCard />
-      <PokemonCard />
-      <PokemonCard />
       <PokemonCard />
       <PokemonCard />
       <PokemonCard />
